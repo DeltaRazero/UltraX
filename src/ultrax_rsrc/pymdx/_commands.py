03e1ec433d034e372cf7ce19994faaa769cb9e57
DeltaRazero <badope6@gmail.com>
2018-09-12 22:18:23 +0200

    At more command object definitions

--- src/ultrax_rsrc/pymdx/_commands.py
+++ src/ultrax_rsrc/pymdx/_commands.py
@@ -23,13 +23,12 @@ class Command:
         self._a = datalist.append    # Set reference to _datatrack->_Data instance
 
     def Rest(self, Clocks):
-        """Rest command."""
+        """Rest command | 休符 コマンド"""
         self._a(Rest(Clocks))
 
     def Note(self, Data, Clocks):
-        """Note command.\n
-        Valid note range: 0x80 - 0xDF\n
-        0x80 = o0d+  /  0xDF = o8d"""
+        """Note command | 音符 コマンド\n
+        Valid note range: 0x80 (o0d+) -- 0xDF (o8d)"""
         self._a(Note(Data, Clocks))
 
 
@@ -38,9 +37,9 @@ class Command:
 
 
 
-#region ||  
+#region ||  MDX Commands  ||
 
-class Rest:
+class Rest:     # 休符データ
     def __init__(self, Clocks):
         self.Clocks = Clocks
 
@@ -53,12 +52,13 @@ class Rest:
         return e
 
 
-class Note:
+class Note:     # 音符データ
     def __init__(self, Data, Clocks):
         self.Data = Data
         self.Clocks = Clocks
 
     def Export(self):
+        # if (0x80 > self.Data > 0xDF  or  self.Clocks < 0): raise AnException
         e = bytearray()
         while (self.Clocks > 256):
             e.extend([0xF7, self.Data, 0xFF])
@@ -67,6 +67,80 @@ class Note:
         return e
 
 
+class Tempo:    # テンポ設定
+    def __init__(self, Data):   # TODO: Calculate BPM to data
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([0xFF, self.Data])
+        return e
+
+
+class OpmRegister:  # OPMレジスタ設定
+    def __init__(self, Register, Data):
+        self.Register = Register
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([self.Register, self.Data])
+        return e
+
+
+class Tone:     # 音色設定
+    def __init__(self, Data):
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([0xFD, self.Data])
+        return e
+
+
+class Pan:      # 出力位相設定
+    def __init__(self, Data):
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([0xFC, self.Data])
+        return e
+
+
+class Volume:   # 音量設定
+    def __init__(self, Data):
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([0xFB, self.Data])
+        return e
+
+
+class VolumeIncrease:   # 音量増大
+    def Export(self):
+        e = bytearray([0xFA])
+        return e
+
+
+class VolumeDecrease:   # 音量減小
+    def Export(self):
+        e = bytearray([0xF9])
+        return e
+
+
+class Gate:     # ゲートタイム
+    def __init__(self, Data):
+        self.Data = Data
+
+    def Export(self):
+        e = bytearray([0xF8, self.Data])
+        return e
+
+
+class Legato:   # Disable keyoff for next note / キーオフ無効
+    def Export(self):
+        e = bytearray([0xF7])
+        return e
+
+
+#endregion
 
 
 
