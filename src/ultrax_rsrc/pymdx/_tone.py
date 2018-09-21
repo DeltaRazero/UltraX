2df90b5956df56b29d60afafc73ce6d71b228792
DeltaRazero <badope6@gmail.com>
2018-09-22 00:15:39 +0200

    Fix tone exporting

--- src/ultrax_rsrc/pymdx/_tone.py
+++ src/ultrax_rsrc/pymdx/_tone.py
@@ -6,7 +6,7 @@ class Tone(opmn.OPMN_Channel):
 
     def __init__(self):
         self.ToneId = 0
-        self.SlotMask = 0
+        self.SlotMask = 15
         opmn.OPMN_Channel.__init__(self)
         return
 
@@ -19,14 +19,20 @@ class Tone(opmn.OPMN_Channel):
 
     def _Export(self):
         """Exports the current tone object to a bytearray."""
-        e = bytearray([self.ToneId, self.Fb, self.Alg, self.SlotMask])
-        for op in self.Op:
-            e.extend([
-                op.Dt1<<4   | op.Mult,
-                op.Tl,
-                op.Ks<<6    | op.Ar,
-                op.Am_On<<7 | op.Dr,
-                op.Dt2<<6   | op.Sr,
-                op.Sl<<4    | op.Rr
-            ])
+        e = bytearray([self.ToneId, self.Fb<<3 | self.Alg, self.SlotMask])
+
+        temp = [[] for _ in range(len(self.Op))]
+        for c, op in enumerate(self.Op):
+            a = temp[c].append
+            a(op.Dt1<<4   | op.Mult)
+            a(op.Tl)
+            a(op.Ks<<6    | op.Ar)
+            a(op.Am_On<<7 | op.Dr)
+            a(op.Dt2<<6   | op.Sr)
+            a(op.Sl<<4    | op.Rr)
+
+        for i in range(len(temp[0])):
+            for j in range(len(temp)):
+                e.append(temp[j][i])
+
         return e
