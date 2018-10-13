de38a4fed117afb4fe6963205428c2d11c8de2ae
DeltaRazero <badope6@gmail.com>
2018-10-14 00:18:03 +0200

    Implement remaining commands + small cleanup / fixing

--- src/ultrax_rsrc/pymdx/_mdx/_commands.py
+++ src/ultrax_rsrc/pymdx/_mdx/_commands.py
@@ -1,9 +1,18 @@
 import struct as struct
 from .._misc._encoding import *
+from .._misc.exc import *
 
-OPM_CLOCK = 4000000 # Hz
+_OPM_CLOCK = 4000000 # Hz
+
+__CMD_EXT       = 0xE7
+__CMD_EXT_02EX  = 0xE6
+# TODO: Make these globals in classes
 
 
+#===========================================#
+#   Standard commands
+#===========================================#
+#region
 class Rest:     # 休符データ
     def __init__(self, Clocks):
         self.Clocks = Clocks
@@ -13,8 +22,11 @@ class Rest:     # 休符データ
 
 class Note:     # 音符データ
     def __init__(self, Data, Clocks):
-        self.Data = Data
-        self.Clocks = Clocks
+        if not(0x80 <= Data <= 0xDF):
+            raise ValueError("Data out of range")
+        else:
+            self.Data = Data
+            self.Clocks = Clocks
     def Export(self):
         # if (0x80 > self.Data > 0xDF  or  self.Clocks < 0): raise AnException
         return bytearray([self.Data, self.Clocks-1])
@@ -26,8 +38,8 @@ class Tempo_Bpm:    # テンポ設定
     def __init__(self, Data):   # TODO: Calculate BPM to data
         self.Data = Data
     def Export(self):
-        global OPM_CLOCK
-        timerb = round(256 - 60 * OPM_CLOCK / (self.Data * 48 * 1024))
+        global _OPM_CLOCK
+        timerb = round(256 - 60 * _OPM_CLOCK / (self.Data * 48 * 1024))
         return bytearray([0xFF, timerb])
 
 
@@ -46,7 +58,7 @@ class OpmControl:  # OPMレジスタ設定
         return bytearray([self.Register, self.Data])
 
 
-class Tone:     # 音色設定
+class Tone:     # 音色設定  //  also doubles as Expdx bank selector - Expdx銀行のセレクタとしても倍増
     def __init__(self, Data):
         self.Data = Data
     def Export(self):
@@ -140,8 +152,32 @@ class DataEnd:
             return bytearray([0xF1, self.Data])
         else:
             e = bytearray([0xF1])
-            e.extend(struct.pack(ENC_WORD, self.Data))
+            e.extend(struct.pack(ENC_WORD, -self.Data))
             return e
+
+
+class DelayKeyon:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        e = bytearray([0xF0])
+        e.append(self.Data)
+        return e
+
+
+class Sync_Resume:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        e = bytearray([0xEF])
+        e.append(self.Data)
+        return e
+
+
+class Sync_Wait:
+    def Export(self):
+        return bytearray([0xEE])
+
 
 
 class AdpcmFreq:
@@ -153,7 +189,154 @@ class AdpcmFreq:
         return e
 
 
-class Pcm8Shift:
+class Expcm_Enable:
     def Export(self):
         return bytearray([0xE8])
 
+
+# :: LFO commands // Pitch ::
+class Lfo_Pitch_Enable:
+    def Export(self):
+        return bytearray([0xEC, 0x81])
+
+class Lfo_Pitch_Disable:
+    def Export(self):
+        return bytearray([0xEC, 0x80])
+
+class Lfo_Pitch_Control:
+    def __init__(self, Wave, Freq, Amp):
+        self.Wave = Wave
+        self.Freq = Freq
+        self.Amp  = Amp
+    def Export(self):
+        e = bytearray([0xEC, self.Wave])
+        e.extend(struct.pack(ENC_WORD, self.Freq))
+        e.extend(struct.pack(ENC_WORD, self.Amp))
+        return e
+
+
+# :: LFO commands // Volume ::
+class Lfo_Volume_Enable:
+    def Export(self):
+        return bytearray([0xEB, 0x81])
+
+class Lfo_Volume_Disable:
+    def Export(self):
+        return bytearray([0xEB, 0x80])
+
+class Lfo_Volume_Control:
+    def __init__(self, Wave, Freq, Amp):
+        self.Wave = Wave
+        self.Freq = Freq
+        self.Amp  = Amp
+    def Export(self):
+        e = bytearray([0xEB, self.Wave])
+        e.extend(struct.pack(ENC_WORD, self.Freq))
+        e.extend(struct.pack(ENC_WORD, self.Amp))
+        return e
+
+
+# :: LFO commands // Opm ::
+class Lfo_Opm_Enable:
+    def Export(self):
+        return bytearray([0xEA, 0x81])
+
+class Lfo_Opm_Disable:
+    def Export(self):
+        return bytearray([0xEA, 0x80])
+
+class Lfo_Opm_Control:
+    def __init__(self, RestartWave, Wave, Speed, Pmd, Amd, Pms_Ams):
+        self.RestartWave = RestartWave
+        self.Wave        = Wave
+        self.Speed       = Speed
+        self.Pmd         = Pmd
+        self.Amd         = Amd
+        self.Pms_Ams     = Pms_Ams
+    def Export(self):
+        return bytearray([0xEA, self.RestartWave*0x40 + self.Wave, self.Speed, self.Pmd, self.Amd, self.Pms_Ams])
+#endregion
+
+
+#===========================================#
+#   Extended Commands (+16)
+#===========================================#
+#region
+class Ext_16_Fadeout:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT, 0x01, self.Data])
+#endregion
+
+
+#===========================================#
+#   Extended Commands (+16/02EX)
+#===========================================#
+#region
+class Ext_16_02EX_RelativeDetune:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        e = bytearray([__CMD_EXT_02EX, 0x01])
+        e.extend(struct.pack(ENC_WORD, self.Data))
+        return e
+
+
+class Ext_16_02EX_Transpose:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT_02EX, 0x02, self.Data])
+
+
+class Ext_16_02EX_RelativeTranspose:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT_02EX, 0x03, self.Data])
+#endregion
+
+
+#===========================================#
+#   Extended Commands (+17)
+#===========================================#
+#region
+class Ext_17_Pcm8_Control:
+    def __init__(self, d0, d1):
+        self.d0 = d0
+        self.d1 = d1
+    def Export(self):
+        e = bytearray([__CMD_EXT, 0x02])
+        e.extend(struct.pack(ENC_WORD, self.d0))
+        e.extend(struct.pack(ENC_WORD, self.d1))
+        return 
+
+
+class Ext_17_UseKeyoff:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT, 0x03, self.Data])
+
+
+class Ext_17_Channel_Control:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT, 0x04, self.Data])
+
+
+class Ext_17_AddLenght:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT, 0x05, self.Data])
+
+
+class Ext_17_UseFlags:
+    def __init__(self, Data):
+        self.Data = Data
+    def Export(self):
+        return bytearray([__CMD_EXT, 0x06, self.Data])
+#endregion
--- src/ultrax_rsrc/pymdx/_mdx/_commands_itf.py
+++ src/ultrax_rsrc/pymdx/_mdx/_commands_itf.py
@@ -1,19 +1,24 @@
 from . import _commands as _cmd
+from .._misc.exc import *
 
 
 class Command:
     """
     MDX performance commands.
     """
-    #region ||  Internal props  ||  
+
+    #===========================================#
+    #   Internal props
+    #===========================================#
+    #region
     def __init__(self, Datalist):
         self._a = Datalist.append    # Set reference to _datatrack->_Data instance
         self._e = Datalist.extend
         self._i = Datalist.insert
 
-        self._rsl = []  # Repeat start,     byte counter list
-        self._rel = []  # Repeat escape,    byte counter list
-        self._lm  = 0   # Loop mark,        byte counter
+        self._rsc = []  # Repeat start,     byte counter list
+        self._rec = []  # Repeat escape,    byte counter list
+        self._lmc = 0   # Loop mark,        byte counter
 
         self.Ext16      = _Command_Ext16(self)
         """MDX performance commands (+16 extension)."""
@@ -26,21 +31,23 @@ class Command:
 
         return
 
+
     def _updateCounters(self, n):
-        if (self._rsl != []):   # Increase repeat start byte counter list
-            for i, _ in enumerate(self._rsl): self._rsl[i]+=n
-        if (self._rel != []):   # Increase repeat escape byte counter list
-            for i, _ in enumerate(self._rel): self._rsl[i]+=n
-        if (self._lm > 0):      # Increase loop mark byte counter
-            self._lm += n
+        if (self._rsc != []):   # Increase repeat start byte counter list
+            for i, _ in enumerate(self._rsc): self._rsc[i]+=n
+        if (self._rec != []):   # Increase repeat escape byte counter list
+            for i, _ in enumerate(self._rec): self._rsc[i]+=n
+        if (self._lmc > 0):      # Increase loop mark byte counter
+            self._lmc += n
         
         return
-
     #endregion
 
 
-    #region ||  Public commands interface  ||
-
+    #===========================================#
+    #   Interface for commands
+    #===========================================#
+    #region
     def Rest(self, Clocks):
         """
         Rest command | 休符 コマンド
@@ -54,7 +61,10 @@ class Command:
         self._updateCounters(1+cmdCount)
         self._a(_cmd.Rest(Clocks))
 
-    def Note(self, Data, Clocks):
+        return
+
+
+    def Note(self, Data, Clocks) -> int:
         """
         Note command | 音符 コマンド\n
         Valid note range: 0x80 (o0d+) -- 0xDF (o8d)
@@ -71,14 +81,20 @@ class Command:
         self._updateCounters(2+cmdCount)
         self._a(_cmd.Note(Data, Clocks))
 
+        return
+
 
     def Tempo_Bpm(self, Data):
         """
-        Tempo command | . . .\n
+        Set the tempo in beats per minute.
+        -----
+        1分あたりの拍数でテンポを設定します。
 
         """
         self._updateCounters(2)
         self._a(_cmd.Tempo_Bpm(Data))
+        return
+
 
     def Tempo_TimerB(self, Data):
         """
@@ -87,14 +103,20 @@ class Command:
         """
         self._updateCounters(2)
         self._a(_cmd.Tempo_TimerB(Data))
+        return
+
 
     def Opm_Control(self, Register, Data):
         self._updateCounters(3)
         self._a(_cmd.OpmControl(Register, Data))
+        return
+
 
     def Tone(self, Data):
         self._updateCounters(2)
         self._a(_cmd.Tone(Data))
+        return
+
 
     def Pan(self, Data):
         self._updateCounters(2)
@@ -116,196 +138,324 @@ class Command:
 
         self._a(_cmd.Pan(Data))
 
+        return
+
+
     def Volume(self, Data):
         self._updateCounters(2)
         self._a(_cmd.Volume(Data))
+        return
+
 
     def Volume_Increase(self):
         self._updateCounters(1)
         self._a(_cmd.Volume_Increase())
+        return
+
 
     def Volume_Decrease(self):
         self._updateCounters(1)
         self._a(_cmd.Volume_Decrease())
+        return
+
 
     def Gate(self, Data):
         self._updateCounters(2)
         self._a(_cmd.Gate(Data))
+        return
+
 
     def Legato(self):
         self._updateCounters(1)
         self._a(_cmd.Legato())
+        return
 
-    def Repeat_Start(self):
-        self._updateCounters(3)
-        self._rsl.append(0)  # New repeat start byte counter
-        
-    def Repeat_End(self, Data):
-        self._updateCounters(3)
-        self._a(_cmd.Repeat_End(self._rsl[-1])) # Add repeat end command
-        self._i(-self._rsl[-1], _cmd.Repeat_Start(Data)) # Insert repeat start command at trackdata index (counted from last) of latest byte counter
-        self._rsl.pop(-1) # Remove the appropriote byte counter
-
-        if (self._rel != []):   # If a repeat escape byte counter is active
-            self._i(-self._rel[-1], _cmd.Repeat_Escape(self._rel[-1])) # Insert repeat escape command at trackdata index (counted from last)
-            self._rel.pop(-1)
-
-    def Repeat_Escape(self):
-        self._updateCounters(3)
-        self._rel.append(0)  # New escape byte counter
 
     def SetDetune(self, Data):
         self._updateCounters(3)
         self._a(_cmd.Detune(Data))
+        return
+
 
     def Portamento(self, Data):
         self._updateCounters(3)
         self._a(_cmd.Portamento(Data))
+        return
+
 
     def DataEnd(self, Data=0x00):
         self._updateCounters(2 if Data==0x00 else 3)
         self._a(_cmd.DataEnd(Data))
+        return
+
 
     def DelayKeyon(self, Data):
         self._updateCounters(2)
         self._a(_cmd.DelayKeyon(Data))
+        return
+
 
     def Sync_Resume(self, Data):
         self._updateCounters(2)
-        self._a(_cmd.SyncResume(Data))
+        self._a(_cmd.Sync_Resume(Data))
+        return
 
-    def Sync_Wait(self, Data):
+
+    def Sync_Wait(self):
         self._updateCounters(1)
-        self._a(_cmd.SyncWait(Data))
+        self._a(_cmd.Sync_Wait())
+        return
+
 
     def AdpcmFreq(self, Data):
         self._updateCounters(2)
         self._a(_cmd.AdpcmFreq(Data))
+        return
 
-
-    # :: LFO commands | Pitch ::
-    def Lfo_Pitch_Enable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Pitch_Enable())
-
-    def Lfo_Pitch_Disable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Pitch_Disable())
-
-    def Lfo_Pitch_Control(self, Wave, Speed, Depth):
-        self._updateCounters(6)
-        self._a(_cmd.Lfo_Pitch_Control(Wave, Speed, Depth))
-
-
-    # :: LFO commands | Volume ::
-    def Lfo_Volume_Enable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Volume_Enable())
-
-    def Lfo_Volume_Disable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Volume_Disable())
-
-    def Lfo_Volume_Control(self, Wave, Speed, Depth):
-        self._updateCounters(6)
-        self._a(_cmd.Lfo_Volume_Control(Wave, Speed, Depth))
-
-
-    # :: LFO commands | Opm ::
-    def Lfo_Opm_Enable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Volume_Enable())
-
-    def Lfo_Opm_Disable(self):
-        self._updateCounters(2)
-        self._a(_cmd.Lfo_Volume_Disable())
-
-    def Lfo_Opm_Control(self, Wave, Speed, Depth):  # TODO: these are wrong input
-        self._updateCounters(7)
-        self._a(_cmd.Lfo_Volume_Control(Wave, Speed, Depth))
-
-
+    
     def Expcm_Enable(self):
         """
         Enable EX-PCM mode |
         """
         self._updateCounters(1)
-        self._a(_cmd.Pcm8Shift())
+        self._a(_cmd.Expcm_Enable())
+        return
 
-#*********************************************************
 
-# /// Extended commands (+16) ///
+    def LoopMark(self):
+        self._lmc = 1
+        return
+
+
+# :: Repeat commands ::
+    def Repeat_Start(self):
+        self._updateCounters(3)
+        self._rsc.append(0)  # New repeat start byte counter
+        return
+
+
+    def Repeat_End(self, Data):
+        self._updateCounters(3)
+
+        # First we add a repeat end command at the end of the trackdata. The loopback point to insert in this
+        # command is the last one added in the 'repeat start' counter ('_rsc'), as if it were to be a 
+        # stack / LIFO buffer.
+        # Then we insert a repeat start command at the index position of the counter we just used.
+        # After that we can safely remove the counter value from the list buffer / free up memory, as we have
+        # no more further use for it anymore.
+
+        # 最初に、トラックデータの最後にrepeat endコマンドを追加します。挿入するループバックポイントの値は、
+        # スタック / LIFOバッファであるかのように、 'repeat start'カウンタ（'_rsc'）に最後に追加されたものです。
+        # 次に、直前に使用したカウンタのインデックス位置にrepeat startコマンドを挿入します。
+        # カウンタバッファの最後の値を使用する必要はもうありません。 したがって、リストバッファ / 空きメモリからカウンタ値を安全に削除できます。
+        
+        self._a(_cmd.Repeat_End(self._rsc[-1]))
+        self._i(-self._rsc[-1], _cmd.Repeat_Start(Data))
+        self._rsc.pop(-1)
+
+        # If the repeat escape byte counter ('_rec') has a value (i.e. is counting), insert a repeat escape command
+        # at the index position from the counter at the last position in the buffer (the same way how we insert repeat
+        # start commands).
+        # After that we can safely remove the counter value from the list buffer / free up memory, as we have no more
+        # further use for it anymore.
+
+        # repeat escapeバイトカウンタ（ '_rec'）に値がある場合（意味：カウント中）、インデックス位置に 'repeat_escape'コマンドが挿入されます。
+        # インデックス位置は、カウンタバッファの最後の位置の値です（リピート開始コマンドの挿入方法と同じです）。
+        # カウンタバッファの最後の値を使用する必要はもうありません。 したがって、リストバッファ/空きメモリからカウンタ値を安全に削除できます。
+
+        if (self._rec != []):
+            self._i(-self._rec[-1], _cmd.Repeat_Escape(self._rec[-1]))
+            self._rec.pop(-1)
+
+        return
+
+
+    def Repeat_Escape(self):
+        self._updateCounters(3)
+        self._rec.append(0)  # New escape byte counter
+        return
+
+
+# :: LFO commands // Pitch ::
+    def Lfo_Pitch_Enable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Pitch_Enable())
+        return
+
+    def Lfo_Pitch_Disable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Pitch_Disable())
+        return
+
+    def Lfo_Pitch_Control(self, Wave, Freq, Amp):
+        self._updateCounters(6)
+        self._a(_cmd.Lfo_Pitch_Control(Wave, Freq, Amp))
+        return
+
+
+# :: LFO commands // Volume ::
+    def Lfo_Volume_Enable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Volume_Enable())
+        return
+
+    def Lfo_Volume_Disable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Volume_Disable())
+        return
+
+    def Lfo_Volume_Control(self, Wave, Freq, Amp):
+        self._updateCounters(6)
+        self._a(_cmd.Lfo_Volume_Control(Wave, Freq, Amp))
+        return
+
+
+# :: LFO commands // Opm ::
+    def Lfo_Opm_Enable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Opm_Enable())
+        return
+
+    def Lfo_Opm_Disable(self):
+        self._updateCounters(2)
+        self._a(_cmd.Lfo_Opm_Disable())
+        return
+
+    def Lfo_Opm_Control(self, Wave, Speed, Pmd=0, Amd=0, Pms=0, Ams=0, RestartWave=0):  # TODO: these are wrong input
+        self._updateCounters(7)
+
+        # Pitch / Amp mod depth low precision
+        Pms_Ams = (Pms<<4 & 0x07) & (Ams & 0x07)
+
+        # PMD / AMS --> Pitch / Amp mod depth high precision
+        # LFRQ --> speed
+        # SYNC --> RestartWave
+        
+        self._a(_cmd.Lfo_Opm_Control(RestartWave, Wave, Speed, Pmd, Amd, Pms_Ams))
+        return
+#endregion
+
+
+#===============================================#
+#   Interface for commands
+#   (+16 extension class)
+#===============================================#
+#region
 class _Command_Ext16:
     def __init__(self, ObjRef):
         self._updateCounters = ObjRef._updateCounters   # Set references to main object
         self._a = ObjRef._a
 
-    def Terminate(self):
-        self._updateCounters(2)
 
     def Fadeout(self, Data):
         self._updateCounters(3)
         self._a(_cmd.Ext_16_Fadeout(Data))
+        return
+#endregion
 
-# /// Extended commands (+16/02EX) ///
+
+#===============================================#
+#   Interface for commands
+#   (+16/02EX extension class)
+#===============================================#
+#region
 class _Command_Ext16_02EX:
     def __init__(self, ObjRef):
         self._updateCounters = ObjRef._updateCounters
         self._a = ObjRef._a
 
-    def Terminate(self):
-        self._updateCounters(2)
 
     def RelativeDetune(self, Data):
         self._updateCounters(4)
         self._a(_cmd.Ext_16_02EX_RelativeDetune(Data))
+        return
+
 
     def Transpose(self, Data):
         self._updateCounters(3)
         self._a(_cmd.Ext_16_02EX_Transpose(Data))
+        return
+
 
     def RelativeTranspose(self, Data):
         self._updateCounters(3)
         self._a(_cmd.Ext_16_02EX_RelativeTranspose(Data))
+        return
+#endregion
 
-# /// Extended commands (+17) ///  ※Not widely supported -- ※広くサポートされていない
+
+#===========================================#
+#   Interface for commands
+#   (+17 extension class)
+#===========================================#
+# ※Not widely supported / ※広くサポートされていない
+#region
 class _Command_Ext17:
     def __init__(self, ObjRef):
         self._updateCounters = ObjRef._updateCounters
         self._a = ObjRef._a
 
-    def Pcm8Control(self):
+    def Pcm8_Control(self, d0, d1):
         self._updateCounters(8)
+        self._a(_cmd.Ext_17_Pcm8_Control(d0, d1))
+        return
 
-    def ___(self):  # ???
+    def UseKeyoff(self, Data):  # Use keyoff y/n
         self._updateCounters(3)
+        self._a(_cmd.Ext_17_UseKeyoff(Data))
+        return
 
-    def ChannelControl(self):
-        self._updateCounters(3)
 
-    def AddLenght(self):
+    def Channel_Control(self, Data):
+        """
+        Control other channel
+
+        Prepares MML command to be inserted into another channel
+
+        -- 
+        """
         self._updateCounters(3)
+        self._a(_cmd.Ext_17_Channel_Control(Data))
+        return
+
+
+    def AddLenght(self, Clocks):
+        cmdCount = 3
+
+        while (Clocks > 256):
+            self._a(_cmd.Ext_17_AddLenght(256))
+            Clocks -= 256
+            cmdCount += 3
+
+        self._updateCounters(cmdCount)
+        self._a(_cmd.Ext_17_AddLenght(Clocks))
+
+        return
+
 
     def Note(self, Data, Clocks):
         """
         Note command (+17 cmds) | 音符 コマンド\n
         Valid note range: 0x80 (o0d+) -- 0xDF (o8d)
         """
-        cmdCount = 2
+        self._updateCounters(2)
 
         if (Clocks > 256):
             self._a(_cmd.Note(Data, 256))
             Clocks -= 256
-            while (Clocks > 256):
-                self._a(_cmd.Ext_17_AddLenght(256))
-                Clocks -= 256
-                cmdCount += 3
+            # The rest of command adding/counting is done in Ext_17_AddLenght
+            self._a(_cmd.Ext_17_AddLenght(Clocks))
+        else:
+            self._a(_cmd.Note(Data, Clocks))
 
-        self._updateCounters(cmdCount)
-        self._a(_cmd.Note(Data, Clocks))
+        return
 
-    def UseFlags(self):
+
+    def UseFlags(self, Data):
         self._updateCounters(3)
+        self._a(_cmd.Ext_17_UseFlags(Data))
+        return
+#endregion
 
 
 
