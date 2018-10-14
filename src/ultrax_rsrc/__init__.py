1b568dfd44f75ebe26f482bb467cbb99eb75cee4
DeltaRazero <badope6@gmail.com>
2018-10-14 23:39:52 +0200

    replace occurrence ntpath with os.path

--- src/ultrax_rsrc/__init__.py
+++ src/ultrax_rsrc/__init__.py
@@ -1,5 +1,6 @@
-import sys, ntpath
-sys.path.append(ntpath.join(sys.path[0], r"ultrax_rsrc"))
+import sys, os
+sys.path.append(os.path.join(sys.path[0], r"ultrax_rsrc"))
 
 from . import pymdx as pymdx
+from . import lang
 #from . import libDeflemask
