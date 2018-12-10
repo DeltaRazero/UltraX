de39fa0ea4b3b2ced915ee7970e2d7d613dc227d
DeltaRazero <badope6@gmail.com>
2018-12-10 20:20:55 +0100

    [Change] pymdx tone object is not dependent on libpyfmtool anymore
    The dependency of libpyfmtool for pymdx's tone object has been removed.
    It has been removed as the library is very imcomplete at the moment of writing
    and has therefor received its own small definition of an OPM tone object.
    
    This does mean future optimizations to tone objects in compiling have to be done
    manually in the font-end of a compiler, atleast until libpyfmtool returns as depedency.

--- src/ultrax_rsrc/pymdx/_misc/_tone.py
+++ src/ultrax_rsrc/pymdx/_misc/_tone.py
@@ -1,13 +1,29 @@
-from libPyFMT import opmn
+
+class _OPM_Operator:
+    Ar, Dr, Sl, Sr, Rr = 0, 0, 0, 0, 0
+    Tl = 0
+    Dt1, Dt2 = 0, 0
+    Mult = 0
+    Ks = 0
+    Am_On = 0
 
 
-class Tone(opmn.OPMN_Channel):
-    """MDX tone definition object."""
+class _OPM_Channel:
 
     def __init__(self):
-        self.ToneId = 0
-        self.SlotMask = 15
-        opmn.OPMN_Channel.__init__(self)
+        self.Fb = 0
+        self.Alg = 0
+        self.Op = [_OPM_Operator() for _ in range(4)]
+
+
+class Tone(_OPM_Channel):
+    """MDX tone definition object."""
+
+    ToneId   = 0
+    SlotMask = 15
+
+    def __init__(self):
+        _OPM_Channel.__init__(self)
         return
 
     def SetTone(self, Patch):
@@ -19,11 +35,13 @@ class Tone(opmn.OPMN_Channel):
 
     def _Export(self):
         """Exports the current tone object to a bytearray."""
-        e = bytearray([self.ToneId, self.Fb<<3 | self.Alg, self.SlotMask])
 
-        temp = [[] for _ in range(len(self.Op))]
+        l_op = len(self.Op)
+        l_param = 6
+
+        tone = [[] for _ in range(l_op)]
         for c, op in enumerate(self.Op):
-            a = temp[c].append
+            a = tone[c].append
             a(op.Dt1<<4   | op.Mult)
             a(op.Tl)
             a(op.Ks<<6    | op.Ar)
@@ -31,8 +49,10 @@ class Tone(opmn.OPMN_Channel):
             a(op.Dt2<<6   | op.Sr)
             a(op.Sl<<4    | op.Rr)
 
-        for i in range(len(temp[0])):
-            for j in range(len(temp)):
-                e.append(temp[j][i])
+        e = bytearray([self.ToneId, self.Fb<<3 | self.Alg, self.SlotMask])
+
+        for param in range(l_param):
+            for op in range(l_op):
+                e.append(tone[op][param])
 
         return e
