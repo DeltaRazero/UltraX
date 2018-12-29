fa1f8833f347472b073758f21258044358198a0d
DeltaRazero <badope6@gmail.com>
2018-12-29 17:59:56 +0100

    [Add] Basic DefleMask .dmf compiler routines

--- src/ultrax_rsrc/uxc/dmf/dmfc.py
+++ src/ultrax_rsrc/uxc/dmf/dmfc.py
@@ -1,165 +1,34 @@
 
-from . import dmf_module
+import io
+from enum import Enum
+
+from . import dmf as dmfm
+from .. import uxc_util
 from ... import pymdx
 
-from enum import Enum
-import io
+from .reader import Channel_Reader, Pattern_Reader
 
 
-
-class UX_CompiledObj:
-    def __init__(self):
-        self.Ticks = [0,0,0,0,0,0,0,0,0]
-        self.Errors = []
-        self.MdxObj = None
-
-
-class Row_Reader:
-    def __init__(self, Channel):
-        self.Patterns = Channel.Patterns
-        self.Sequence = Channel.Sequence
-
-        self.Seq_Index = 0
-        self.Row_Index = 0
-
-        self.Note_Active = False
-
-
-    def Seek(self, Seq_Index, Row_Index)
-
-
-
-    def Read(self):
-        # uint8_t   seq_index;
-        # uint8_t   row_index;
-
-        # bool   position_jump;
-        # bool   pattern_break;
-
-        if (Seq_Index):
-            seq_index = Seq_Index
-        else:
-            seq_index = 0
-
-        if (Row_Index):
-            pattern_break = True
-            row_index = Row_Index
-        else:
-            pattern_break = False
-
-        # Iterate through patterns in sequence order
-        while (seq_index <= len(self.Sequence)):
-            seq = self.Sequence[seq_index]
-
-            position_jump = False
-            for pattern in self.Patterns[seq]:
-
-                if (position_jump): break
-                if not (pattern_break): row = 0
-
-                pattern_break = False
-                while (row_index <= len(pattern.Rows)):
-                    row = pattern.Rows[row_index]
-
-                    if (position_jump or pattern_break): break
-
-                    for fx in row.Fx:
-
-                        if (fx.Code == 0x0B):
-                            if (fx.Value > seq_index):
-                                seq_index = fx.Value
-                                position_jump = True
-
-                        elif (fx.Code == 0x0D):
-                            row_index = fx.Value
-                            pattern_break = True
-                        
-                    yield row
-                    row_index += 1
-
-            seq_index += 1
-
-
-class PatternReader:
-
-
-
-
-class Channel_Reader:
-    def __init__(self, Channel):
-        
-
-        self.IsDuplicate = False # True when directly looping
-
-        self.CurrentIns  = 0
-        self.CurrentPtrn = 0
-
-        self.Patterns = Channel.Patterns
-        self.Sequence = Channel.Sequence
-
-        #self.Seq_Index = 0
-        #self.Row_Index = 0
-
-
-    def Create_RowReader(self, Seq_Index=None, Row_Index=None):
-        return Row_Reader(self, Seq_Index, Row_Index)
-
-
-    # NOTE: make class instead?
-    def RowReader(self, Seq_Index=None, Row_Index=None):
-        # uint8_t   seq_index;
-        # uint8_t   row_index;
-
-        # bool   position_jump;
-        # bool   pattern_break;
-
-        if (Seq_Index):
-            seq_index = Seq_Index
-        else:
-            seq_index = 0
-
-        if (Row_Index):
-            pattern_break = True
-            row_index = Row_Index
-        else:
-            pattern_break = False
-
-        # Iterate through patterns in sequence order
-        while (seq_index <= len(self.Sequence)):
-            seq = self.Sequence[seq_index]
-
-            position_jump = False
-            for pattern in self.Patterns[seq]:
-
-                if (position_jump): break
-                if not (pattern_break): row = 0
-
-                pattern_break = False
-                while (row_index <= len(pattern.Rows)):
-                    row = pattern.Rows[row_index]
-
-                    if (position_jump or pattern_break): break
-
-                    for fx in row.Fx:
-
-                        if (fx.Code == 0x0B):
-                            if (fx.Value > seq_index):
-                                seq_index = fx.Value
-                                position_jump = True
-
-                        elif (fx.Code == 0x0D):
-                            row_index = fx.Value
-                            pattern_break = True
-                        
-                    yield row
-                    row_index += 1
-
-            seq_index += 1
-
-
+#rowtick = 0x18
+rowtick = 0x06
 
 
 class UXC_Dmf:
+
+
+    VALUES_STEREO = {
+        0x10: 0b01,  0xF0: 0b01,
+        0x11: 0b11,  0xFF: 0b11,
+        0x01: 0b10,  0x0F: 0b10,
+        0x00: 0b00
+    }
+
+    EMPTY_FX_CMDS = [ [], [] ]
+
+
+
+
+    global rowtick
 
     def __init__(self):
         pass
@@ -170,7 +39,7 @@ class UXC_Dmf:
         #self.SetCompilerOptions(Arguments)
 
         # Load and parse DefleMask module
-        dmf = dmf_module.Dmf()
+        dmf = dmfm.Dmf()
         dmf.Load(Path)
 
         # argparse thing
@@ -182,86 +51,237 @@ class UXC_Dmf:
         # Create mdx object
         mdx = pymdx.Mdx(UseExpcm)
 
+        # Set header data
+        mdx.Header.Title = dmf.Header.SongName + " - " + dmf.Header.SongAuthor
 
         # Set tones
         for c, ins in enumerate(dmf.Instruments):
-
             tone = self._ConvertInstrument_FM(ins)
             tone.ToneId = c
             mdx.Tones.append(tone)
 
+        # Set speed related variables
+        mod_tb = dmf.Module.TimeBase + 1
 
-        # Calculate the initial tempo
-        tempo = CalcTempo(dmf.Module.Tick1)
-        mdx.DataTracks[0].Add.Tempo_Bpm(tempo)
-
+        if (dmf.Module.UseCustomHz):
+            mod_hz = dmf.Module.CustomHz
+        else:
+            if (dmf.Module.Framemode == 0):
+                mod_hz = 50
+            elif (dmf.Module.Framemode == 1):
+                mod_hz = 60
+            else:
+                raise Exception
 
         if (UseExpcm):
-            self.Channels = [Channel_Reader(chn) for chn in dmf.Module.Channels]
+            amount_chn = 13
         else:
-            self.Channels = [Channel_Reader(chn) for chn in dmf.Module.Channels[:9]]
+            amount_chn = 9
+
+        cr = [Channel_Reader(channel) for channel in dmf.Module.Channels[:amount_chn]]
 
 
-        r = [chn.RowReader() for chn in self.Channels]
-        for c, channel in enumerate(self.Channels):
-            
-            row = next(r[c])    # r[c].GetRow()
+        # Calculate the initial tempo
+        
+        tempo = CalcTempo(dmf.Module.Tick1, mod_hz, mod_tb)
+        #tempo = 240
+        mdx.DataTracks[0].Add.Tempo_Bpm(tempo)
 
-
-            if (row.Note is 0):
-                pass
-
-            elif (row.Note is 100): # not needed
-                pass
-
-            else:
-                notereader = channel.RowReader()
-                
-                mdx.DataTracks[c].Add.Note(note, clocks)
-
-
-
-            if (row.Note != 0):
-
-
-            #if 
-
-            
-            rr = channel.RowReader()
-            #while :
-
-
-
-               # next(rr, None)
-
-
-
-
-
-
-
-            
+        # Init channel-pattern positions
+        for c, channel in enumerate(cr):
+            mdx.DataTracks[c].Add.Volume(0x80)
+            channel.Volume = 0x7F
+            channel.Bpm = tempo
             
 
-        for _ in range(dmf.Module.TOTAL_ROWS_PER_PATTERN):
-            for channel in self.Channels:
+        done = [False for _ in range(len(cr))]
+        #done = [True for _ in range(len(cr))]
+        #done[7] = False
+        while not all(done):
 
-                channel.next()
+            # Effects/commands
+            special_cmds = [ [], [] ]
+
+            # Special commands
+            # for c, channel in enumerate(cr):
+
+            #     if (done[c]):
+            #         continue
+
+            #     row = channel.Pattern.Row
+
+            #     for fx in row.Fx:
+            #     # FX commands only for YM2151
+            #         if (fx.Code == )
 
 
+            # if tempo change
+            #   if Porta not None / if channel.Porta
+            #       readadjust porta shit
 
-            data = mdx.DataTracks[i_chn]
-            pattern = chn.Patterns[ptrnIndex]
+            # actual parsing shit here
+            for c, channel in enumerate(cr):
 
-            ptrnRepeats = _C_PatternRepeats(i, chn.PatternOrder)
+                #print("channel {} : position {}, pattern {} : row {}".format(c+1, channel.Pattern.Position, channel.Pattern.PatternId, channel.Pattern.Position) )
 
-                # Check fx
+                #if (c==7 and channel.Pattern.PatternId==11 and channel.Pattern.Position==11):
+                #    print("fuck off")
 
-            for row in pattern.Rows:
-                pass
+                if (done[c]):
+                    continue
+
+                #a = mdx.DataTracks[0].Add.Re
+                #b = mdx.DataTracks[0].
+
+                row = channel.Pattern.Row
+
+                # Effects/commands
+                fx_cmds = [ [], [] ]
+
+                if not (row.Instr == -1):
+                    if not (row.Instr is channel.Instrument):
+                        fx_cmds[0].append(pymdx.command.Tone(row.Instr))
+                        channel.Instrument = row.Instr
+
+                if not (row.Volume == -1):
+                    if not (row.Volume is channel.Volume):
+                        volume = (-(row.Volume - 0x7F)) + 0x80
+                        fx_cmds[0].append(pymdx.command.Volume(volume))
+                        channel.Volume = row.Volume
+
+                for fx in row.Fx:
+                    # FX commands only for YM2151
+                    if (c < 8):
+
+                        # Vibrato
+                        if (fx.Code == 0x04):
+                            pass
+
+                        # Stereo/pan
+                        if (fx.Code == 0x08):
+                            if (fx.Value in self.VALUES_STEREO):
+                                fx_cmds[0].append(pymdx.command.Pan(self.VALUES_STEREO[fx.Value]))
+                            else:
+                                raise Exception
+
+                        # Porta commands
+                        elif (fx.Code == 0x01  or  fx.Code == 0x02  or  fx.Code == 0x03):
+
+                            if (fx.Value == 0  or  fx.Value == -1):
+                                channel.Porta = None
+                                channel.PortaCount = 0
+
+                            else:
+                                # TODO: The calculation is probably incorrect
+                                mdx_hz = channel.Bpm / 60
+                                multiplier = mod_hz / mdx_hz
+                                value = round(fx.Value * (rowtick * dmf.Module.Tick1) * multiplier)
+                                value = pymdx._misc._util.Clamp(value, -32768, 32767)
+
+                                # TODO: Note actions are a bit bugged with portas
+
+                                # Porta up
+                                if (fx.Code == 0x01):
+                                    fx_cmds[0].append(pymdx.command.Portamento(value))
+
+                                # Porta down
+                                elif (fx.Code == 0x02):
+                                    fx_cmds[0].append(pymdx.command.Portamento(-value))
+
+                                #channel.Porta = fx_cmds[0][-1]
+                                # Porta up
+                                #elif (fx.Code == 0x03):
+                                #    fx_cmds[0].append(pymdx.command.Portamento(fx.Value * multiplier))
+                                #    fx_cmds[1].append(pymdx.command.Note(0x80, 0))
+                                    # channel.new.portacounter()
 
 
-        uxobj = UX_CompiledObj()
+                # Row Note actions
+                # Empty
+                if (row.Note == 0 and row.Octave == 0):
+                    # If first row in module
+                    if (channel.Note is None):
+                        mdx.DataTracks[c].Extend(fx_cmds[0])
+                        mdx.DataTracks[c].Add.Rest(rowtick)
+                        channel.Note = mdx.DataTracks[c].Data[-1]
+                    else:
+                        if (fx_cmds != self.EMPTY_FX_CMDS):
+                            if (channel.NoteActive):
+                                #mdx.DataTracks[c].Data.insert(-1, pymdx.command.Legato() ) # TODO: make this better, not use insert?
+                                #mdx.DataTracks[c].Add.Legato()
+                                mdx.DataTracks[c].Extend(fx_cmds[0])
+                                if (fx_cmds[1] != []):
+                                    mdx.DataTracks[c].Extend(fx_cmds[1])
+                                else:
+                                    mdx.DataTracks[c].Add.Note(channel.Note.Data, 0)
+                            else:
+                                mdx.DataTracks[c].Extend(fx_cmds[0])
+                                mdx.DataTracks[c].Add.Rest(0)
+                            # Set reference to current Note/Rest
+                            channel.Note = mdx.DataTracks[c].Data[-1]
+
+                        channel.Note.Clocks += rowtick
+                        #print(channel.Note.Clocks)
+
+                else:
+                # Note OFF
+                    if (row.Note == 100):
+                        if (channel.NoteActive):
+                            if (fx_cmds != []): mdx.DataTracks[c].Extend(fx_cmds[0])
+                            mdx.DataTracks[c].Add.Rest(rowtick)
+                            channel.NoteActive = False
+                            channel.Note = mdx.DataTracks[c].Data[-1]
+                        # Note OFF when note already OFF
+                        else:
+                            if not (channel.Note is None):
+                                channel.Note.Clocks += rowtick
+                # Note ON
+                    else:
+                        if (fx_cmds != []): mdx.DataTracks[c].Extend(fx_cmds[0])
+                        note = row.Note + (12 * row.Octave) + 0x7D
+                        mdx.DataTracks[c].Add.Note(note, rowtick)
+                        channel.NoteActive = True
+                        channel.Note = mdx.DataTracks[c].Data[-1]
+
+
+            # Read next row into reader objects
+            for c, channel in enumerate(cr):
+
+                # Check whether channel is done
+                if (done[c]):
+                    continue
+
+                pattern = channel.Pattern # Current pattern
+                pattern.AdvanceRow()
+
+                if (pattern.Row is None):
+                    channel.AdvancePattern()
+                    pattern = channel.Pattern # Current pattern
+                    # If all patterns done/read
+                    if (pattern is None):
+                        done[c] = True
+                        continue
+                    
+        
+        # Cut off any notes, if active
+        for c, channel in enumerate(cr):
+            if (channel.NoteActive):
+                mdx.DataTracks[c].Add.Rest(5)
+
+
+        #mdx.DataTracks[0].Add.Repeat_End()
+
+
+        # pdx = pymdx.Pdx()
+
+        # for i, sample in enumerate(dmf.Samples):
+        #     pdx.Banks[0].Samples[i].Rate = sample.Rate
+        #     pdx.Banks[0].Samples[i].Data = sample.Data
+
+        # pdx_exported = pdx.Export()
+
+
+        uxobj = uxc_util.UX_CompiledObj()
         uxobj.MdxObj = mdx
 
         return uxobj
@@ -285,14 +305,14 @@ class UXC_Dmf:
 
             tone.Op[op].Tl = ins.Data.Op[op].Tl
 
-            tone.Op[op].Dt1 = ins.Data.Op[op].Dt1
+            tone.Op[op].Dt1 = ins.Data.Op[op].Dt
             tone.Op[op].Dt2 = ins.Data.Op[op].Dt2
 
             tone.Op[op].Mult = ins.Data.Op[op].Mult
 
             tone.Op[op].Ks = ins.Data.Op[op].Ks
 
-            tone.Op[op].Am_On = ins.Data.Op[op].Am_On
+            tone.Op[op].Am_On = ins.Data.Op[op].Am
 
         return tone
 
@@ -315,95 +335,6 @@ class UXC_Dmf:
         pass
 
 
-    def RowReader(self, Channel: dmf_module._Channel, Seq_Index=None, Row_Index=None) -> dmf_module._Row:
-
-        # uint8_t   seq_index;
-        # uint8_t   row_index;
-
-        # bool   position_jump;
-        # bool   pattern_break;
-
-        if (Seq_Index):
-            seq_index = Seq_Index
-        else:
-            seq_index = 0
-
-        if (Row_Index):
-            pattern_break = True
-            row_index = Row_Index
-        else:
-            pattern_break = False
-
-        # Iterate through patterns in sequence order
-        while (seq_index <= len(Channel.Sequence)):
-            seq = Channel.Sequence[seq_index]
-
-            position_jump = False
-            for pattern in Channel.Patterns[seq]:
-
-                if (position_jump): break
-                if not (pattern_break): row = 0
-
-                pattern_break = False
-                while (row_index <= len(pattern.Rows)):
-                    row = pattern.Rows[row_index]
-
-                    if (position_jump or pattern_break): break
-
-                    for fx in row.Fx:
-
-                        if (fx.Code == 0x0B):
-                            if (fx.Value > seq_index):
-                                seq_index = fx.Value
-                                position_jump = True
-
-                        elif (fx.Code == 0x0D):
-                            row_index = fx.Value
-                            pattern_break = True
-                        
-                    yield row
-                    row_index += 1
-
-            seq_index += 1
-
-
-
-    
-    def Semantic(self, dmf: dmf_module.Dmf):
-
-        # Set jump positions
-        rr = [self.RowReader(channel) for channel in dmf.Module.Channels]
-
-        l_channel = len(dmf.Module.Channels)
-        for channel in dmf.Module.Channels:
-            
-            for seq in channel.Sequence:
-
-                for pattern in channel.Patterns[seq]:
-
-                    for i in range(l_channel):
-
-                        row = next(rr, None)
-                        
-                        for fx in row.Fx:
-
-                            if fx.Code == 1:
-                                ""
-
-        for i in rr:
-            row = next(i, None)
-
-
-            
-
-        #dmf.Module.
-
-
-        #except StopIteration
-
-
-
-        return 
 
 
 
@@ -414,16 +345,16 @@ class UXC_Dmf:
 # beat_duration = pattern_tick * amount_rows_for_beat (8)
 # bpm = 60 / beat_duration
 
-def CalcTempo(tickspeed, refresh=60):
+def CalcTempo(tickspeed, refresh=60, basetime=1):
     row_duration  = tickspeed / refresh
     beat_duration = row_duration * 8
-    bpm = 60 / beat_duration
+    bpm = (60 / beat_duration) / basetime
     return round(bpm)
 
 def UsesExpcm(dmf):
-    for channel in range(8, 12):
-        for seq in channel.Sequence:
-            for row in channel.Patterns[seq].Rows:
+    for c in range(8, 12):
+        for seq in dmf.Module.Channels[c].Sequence:
+            for row in dmf.Module.Channels[c].Patterns[seq].Rows:
                 if (row.Note != 0 and row.Octave != 0):
                     return True
 
@@ -431,6 +362,15 @@ def UsesExpcm(dmf):
                     if (fx.Code != -1 or fx.Value != -1):
                         return True
     return False
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/ultrax_rsrc/uxc/dmf/reader.py
@@ -0,0 +1,100 @@
+
+
+from . import dmf
+from ... import pymdx
+
+
+
+class Pattern_Reader:
+    """Reads patterns"""
+
+    PatternId = None
+    _Rows = []
+
+    # dmf_module._Pattern
+    Row : dmf._Row = None # Current row ref
+    Position : int = None
+
+
+    def __init__(self, Pattern):
+        self._Rows = Pattern.Rows
+        self.PatternId = Pattern.Id
+        self.Position = 0
+        self.Row = self._Rows[self.Position]
+        return
+
+    def SeekRow(self, Position, Mode=0):
+        #Mode: absolute, relative
+        if Mode is 0:
+            self.Position = Position
+
+        elif Mode is 1:
+            self.Position += Position
+
+        self._SetRow(Position)
+        return
+
+    def _SetRow(self, Position):
+        self.Row = self._Rows[self.Position]
+        return
+
+    def AdvanceRow(self):
+        self.Position += 1
+            
+        if (self.Position > len(self._Rows)-1 ):
+            self.Row = None
+        else:
+            self.Row = self._Rows[self.Position]
+        return
+
+
+class Channel_Reader:
+    """Reads channels"""
+
+
+    NoteActive : bool = False
+    Note : pymdx.command.Note = None    # Reference to current note command obj
+    Instrument : int = None
+    Volume : int = None
+
+    Porta : pymdx.command.Portamento = None
+    PortaCount = 0
+
+    Bpm : int = 0
+
+    Pattern : Pattern_Reader = None # Reference to patternreader object
+    Position : int = None
+
+    _Patterns = {}
+    _Sequence = []
+
+    def __init__(self, DmfChannel_Obj):
+        self._Patterns = DmfChannel_Obj.Patterns
+        self._Sequence = DmfChannel_Obj.Sequence
+        self.Position = 0
+        self.Pattern = Pattern_Reader(self._Patterns[self._Sequence[self.Position]])
+        return
+
+    def SeekPattern(self, Position, Mode=0):
+        #Mode: absolute, relative
+        if Mode is 0:
+            self.Position = Position
+
+        elif Mode is 1:
+            self.Position += Position
+
+        self._SetPattern(Position)
+        return
+
+    def _SetPattern(self, Position):
+        self.Pattern = Pattern_Reader(self._Patterns[self._Sequence[Position]])
+        return
+
+    def AdvancePattern(self):
+        self.Position += 1
+
+        if (self.Position > len(self._Sequence)-1 ):
+            self.Pattern = None
+        else:
+            self.Pattern = Pattern_Reader(self._Patterns[self._Sequence[self.Position]])
+        return
